d13ebb4b1becb4ad47ef0f18376d0f189fa4433f
Bartłomiej Płóciennik <bplociennik03@gmail.com>
2024-11-04 08:31:13 +0100

    tracking authentication status

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -7,11 +8,29 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text("Hello"),
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+          if (!snapshot.hasData) {
+            return Placeholder();
+          }
+          return Center(
+            child: Text("test"),
+          );
+        },
       ),
-    ),
-  ));
+    );
+  }
 }
