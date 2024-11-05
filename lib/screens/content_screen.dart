aaffe832549c0c3bc19f48459aa968811013ff5f
Bartłomiej Płóciennik <bplociennik03@gmail.com>
2024-11-05 10:00:34 +0100

    reading provider in content page

--- lib/screens/content_screen.dart
+++ lib/screens/content_screen.dart
@@ -1,23 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:remember/helpers/functions.dart';
+import 'package:remember/helpers/providers.dart';
 import 'package:remember/widgets/custom_app_bar.dart';
 
-class ContentScreen extends StatefulWidget {
+class ContentScreen extends ConsumerStatefulWidget {
   const ContentScreen({super.key});
 
   @override
-  State<ContentScreen> createState() => _ContentScreenState();
+  ConsumerState<ContentScreen> createState() => _ContentScreenState();
 }
 
-class _ContentScreenState extends State<ContentScreen> {
+class _ContentScreenState extends ConsumerState<ContentScreen> {
   int counter = 0;
+  int currentIndex = 0;
+
+  Widget get currentContent {
+    return Placeholder();
+  }
+
   @override
   Widget build(BuildContext context) {
+    currentIndex = ref.watch(indexProvider);
+    print(currentIndex);
+
     return Scaffold(
       bottomNavigationBar: kIsWeb ? null : const CustomAppBar(),
       appBar: AppBar(
@@ -40,6 +49,7 @@ class _ContentScreenState extends State<ContentScreen> {
         actions: [
           IconButton(
             onPressed: () {
+              ref.read(indexProvider.notifier).state = 0;
               FirebaseAuth.instance.signOut();
             },
             icon: const Icon(Icons.logout),
