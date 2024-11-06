0864f9d343ee5b70c2d19291dfb11a91f6b15f9f
Bartłomiej Płóciennik <bplociennik03@gmail.com>
2024-11-06 14:51:24 +0100

    preparations for url handling

--- lib/widgets/new_memory/new_location_widget.dart
+++ lib/widgets/new_memory/new_location_widget.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:remember/helpers/functions.dart';
 import 'package:remember/screens/map_screen.dart';
 import 'package:remember/widgets/new_memory/new_memory_container.dart';
+import 'package:http/http.dart' as http;
 
 class NewLocationWidget extends StatefulWidget {
   const NewLocationWidget({super.key});
@@ -17,11 +21,17 @@ class _NewLocationWidgetState extends State<NewLocationWidget> {
       text: "Nie podano lokalizacji",
       icon: Icons.location_off,
       onTap: () async {
-        final location = await Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => MapScreen(isSelecting: true),
+        /* final location = await Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const MapScreen(isSelecting: true),
         ));
-        print(location);
-        if (location == null) return;
+
+        if (location == null) return;*/
+        final status = await checkConnection();
+        if (!status) return;
+        final url = Uri.parse('https://httpbin.org/ip');
+        final response = await http.get(url);
+        final resData = json.decode(response.body);
+        final address = resData['results'][0]['formatted_address'];
       },
     );
   }
